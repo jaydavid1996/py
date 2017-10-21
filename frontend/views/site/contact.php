0df88c51e6e24bf7c9a01c13f341e2de00a31729
ajzamora <ajzamora@users.noreply.github.com>
2017-10-22 01:03:35 +0800

    CHANGE contact and login fields to polymer elements (unfinished)

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -1,3 +1,7 @@
+<link rel="import" href="vendor/bower/paper-input/paper-input.html">
+<link rel="import" href="vendor/bower/paper-input/paper-textarea.html">
+<link rel="import" href="vendor/bower/paper-button/paper-button.html">
+
 <?php
 
 /* @var $this yii\web\View */
@@ -15,27 +19,43 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
+        If you have inquiries, please fill out the following form to contact us. Thank you.
     </p>
 
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
+                <?= $form->field($model, 'name', [
+                  'template' => '<paper-input autofocus tabindex=1  label="Name" id="contactform-name" name="ContactForm[name]" required auto-validate error-message="Name cannot be blank"></paper-input>'
+                ]); ?>
 
-                <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'email', [
+                  'template' => '<paper-input tabindex=2  label="Email" id="contactform-email" name="ContactForm[email]" required auto-validate error-message="Email cannot be blank"></paper-input>'
+                ]); ?>
 
-                <?= $form->field($model, 'email') ?>
+                <?= $form->field($model, 'subject', [
+                  'template' => '<paper-input tabindex=3  label="Subject" id="contactform-subject" name="ContactForm[subject]" required auto-validate error-message="Subject cannot be blank"></paper-input>'
+                ]); ?>
 
-                <?= $form->field($model, 'subject') ?>
+                <?= $form->field($model, 'body', [
+                  'template' => '<paper-textarea tabindex=4  char-counter maxlength="300" label="Body" id="contactform-body" name="ContactForm[body]" required auto-validate error-message="Body cannot be blank"></paper-textarea>'
+                ]); ?>
 
-                <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+                <!-- <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?> -->
+
+                <!-- <?= $form->field($model, 'email') ?> -->
+
+                <!-- <?= $form->field($model, 'subject') ?> -->
+
+                <!-- <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?> -->
 
                 <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+                    'template' => '<div class="row"><div class="col-lg-5"><paper-input tabindex=5  label="Verification Code" id="contactform-verifycode" name="ContactForm[verifyCode]" required auto-validate error-message="The verification code is incorrect."></paper-input></div><div class="col-lg-3">{image}</div></div>',
                 ]) ?>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                    <paper-button name="contact-button" onclick="submitContact()" raised>Submit</paper-button>
+                    <!-- <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?> -->
                 </div>
 
             <?php ActiveForm::end(); ?>
@@ -43,3 +63,10 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
 
 </div>
+<script>
+// var username = document.getElementById('loginform-username');
+// var password = document.getElementById('loginform-password');
+function submitContact() {
+    document.getElementById('contact-form').submit();
+}
+</script>
--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -1,3 +1,7 @@
+<link rel="import" href="vendor/bower/paper-input/paper-input.html">
+<link rel="import" href="vendor/bower/paper-button/paper-button.html">
+<link rel="import" href="vendor/bower/paper-checkbox/paper-checkbox.html">
+
 <?php
 
 /* @var $this yii\web\View */
@@ -17,23 +21,55 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="row">
         <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+            <!-- <paper-input-container always-float-label auto-validate attr-for-value="value"> -->
+            <!-- <label slot="label">Social Security Number</label> -->
+            <!-- <paper-input-error slot="add-on">SSN invalid!</paper-input-error> -->
+            <!-- </paper-input-container> -->
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+            <?php $form = ActiveForm::begin(['id' => 'login-form',
+                'fieldConfig' => [
+                'options' => [
+                'tag' => false,
+            ],
+            // 'enableClientValidation' => false,
+            // 'enableAjaxValidation' => false,
+            ],]); ?>
+                <?= $form->field($model, 'username', [
+                    'template' => '<paper-input autofocus tabindex=1 label="Username" id="loginform-username" name="LoginForm[username]" required auto-validate error-message="Username cannot be blank"></paper-input>'
+                ]); ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+                <?= $form->field($model, 'username', [
+                    'template' => '<paper-input tabindex=2 label="Password" id="loginform-password" name="LoginForm[password]" required auto-validate error-message="Password cannot be blank"></paper-input>'
+                ]); ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                <!-- <?= $form->field($model, 'rememberMe', [
+                    'template' => '<paper-checkbox id="loginform-rememberme" name="LoginForm[rememberMe]" required auto-validate error-message="Password cannot be blank">{label}</paper-checkbox>'
+                ]); ?> -->
+
+                <!-- <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?> -->
+
+                <!-- <?= $form->field($model, 'password')->passwordInput() ?> -->
+
+                <!-- <?= $form->field($model, 'rememberMe')->checkbox() ?> -->
 
                 <div style="color:#999;margin:1em 0">
                     If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
                 </div>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
+                <!-- <div class="form-group"> -->
+                    <paper-button name="login-button" onclick="submitLogin()" raised>login</paper-button>
+                    <!-- <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?> -->
+                <!-- </div> -->
 
             <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
+
+<script>
+// var username = document.getElementById('loginform-username');
+// var password = document.getElementById('loginform-password');
+function submitLogin() {
+    document.getElementById('login-form').submit();
+}
+</script>
