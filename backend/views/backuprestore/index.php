1767adf31c30acf17dc07c1f88cd7c3645b4b5f3
ajzamora <ajzamora@users.noreply.github.com>
2017-10-22 15:09:39 +0800

    ADD MVC backup and restore

---
+++ backend/views/backuprestore/index.php
@@ -0,0 +1,31 @@
+<div class="backup-default-index">
+
+    <?php
+    $this->params ['breadcrumbs'] [] = [
+        'label' => 'Manage',
+        'url' => array(
+            'index'
+        )
+    ];
+    ?>
+<!--This section comment to not duplicate the flash message-->
+    <!-- <?php if (Yii::$app->session->hasFlash('success')): ?>
+        <div class="alert alert-success">
+            <?php echo Yii::$app->session->getFlash('success'); ?>
+        </div>
+    <?php endif; ?> -->
+    <!--End -->
+
+
+
+    <div class="row">
+        <div class="col-md-12">
+            <?php
+            echo $this->render('_list', array(
+                'dataProvider' => $dataProvider
+            ));
+            ?>
+        </div>
+    </div>
+
+</div>
