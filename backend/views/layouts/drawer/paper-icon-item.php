a92e5b26a1a90d7b504d432c35b621945803fc03
dennisbello <dbc0429@gmail.com>
2017-10-22 15:40:51 +0800

    MODIFY backend sidenavigation urls

--- backend/views/layouts/drawer/paper-icon-item.php
+++ backend/views/layouts/drawer/paper-icon-item.php
@@ -45,6 +45,7 @@
   </style>
 </custom-style>
 
+
 <?PHP
 
 $baseUrl = Yii::$app->homeUrl
@@ -63,55 +64,55 @@ $baseUrl = Yii::$app->homeUrl
   <span>Gallery</span>
 </paper-icon-item>
 </a>
-<a href="<?php echo $baseUrl?>users/" tabindex="-1">
+<a href="<?php echo $baseUrl?>user/" tabindex="-1">
 <paper-icon-item class="iconItem">
   <iron-icon class="grayIcon" icon="icons:account-circle" slot="item-icon"></iron-icon>
   <span>Users</span>
 </paper-icon-item>
 </a>
-<a href="<?php echo $baseUrl?>" tabindex="-1">
+<a href="<?php echo $baseUrl?>report/" tabindex="-1">
 <paper-icon-item class="iconItem">
   <iron-icon class="grayIcon" icon="icons:assignment" slot="item-icon"></iron-icon>
   <span>Reports</span>
 </paper-icon-item>
 </a>
-<a href="<?php echo $baseUrl?>" tabindex="-1">
+<a href="<?php echo $baseUrl?>audit/" tabindex="-1">
 <paper-icon-item class="iconItem">
   <iron-icon class="grayIcon" icon="icons:trending-up" slot="item-icon"></iron-icon>
   <span>Audit Trails</span>
 </paper-icon-item>
 </a>
-<a href="<?php echo $baseUrl?>" tabindex="-1">
+<a href="<?php echo $baseUrl?>backuprestore/" tabindex="-1">
 <paper-icon-item class="iconItem">
   <iron-icon class="grayIcon" icon="icons:settings-backup-restore" slot="item-icon"></iron-icon>
   <span>Backup & Restore</span>
 </paper-icon-item>
 </a>
-<a href="<?php echo $baseUrl?>" tabindex="-1">
+<a href="<?php echo $baseUrl?>occasion/" tabindex="-1">
 <paper-icon-item class="iconItem">
   <iron-icon class="grayIcon" icon="av:album" slot="item-icon"></iron-icon>
-  <span>Occasion</span>
+  <span>Occasions</span>
 </paper-icon-item>
 </a>
-<a href="<?php echo $baseUrl?>" tabindex="-1">
+<a href="<?php echo $baseUrl?>event/" tabindex="-1">
 <paper-icon-item class="iconItem">
   <iron-icon class="grayIcon" icon="av:explicit" slot="item-icon"></iron-icon>
   <span>Events</span>
 </paper-icon-item>
 </a>
-<a href="<?php echo $baseUrl?>" tabindex="-1">
+<a href="<?php echo $baseUrl?>player/" tabindex="-1">
 <paper-icon-item class="iconItem">
   <iron-icon class="grayIcon" icon="social:group-add" slot="item-icon"></iron-icon>
   <span>Players</span>
 </paper-icon-item>
 </a>
-<a href="<?php echo $baseUrl?>" tabindex="-1">
+<a href="<?php echo $baseUrl?>venue/" tabindex="-1">
 <paper-icon-item class="iconItem">
   <iron-icon class="grayIcon" icon="social:domain" slot="item-icon"></iron-icon>
   <span>Venue</span>
 </paper-icon-item>
 </a>
-<a href="<?php echo $baseUrl?>" tabindex="-1">
+<a href="<?php echo $baseUrl?>report" tabindex="-1">
 <paper-icon-item class="iconItem">
   <iron-icon class="grayIcon" icon="icons:assignment" slot="item-icon"></iron-icon>
   <span>Reports</span>
