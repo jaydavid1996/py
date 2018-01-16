de0ea4bf436f37fed1bbfeedaa58babfb5f88072
ajzamora2 <ajzamora@users.noreply.github.com>
2018-01-16 20:36:21 +0800

    ADD Double Elimination Templates (3 to 12 teams)

---
+++ frontend/views/site/4team.php
@@ -0,0 +1,64 @@
+<h1>4 TEAM DOUBLE ELIMINATION</h1>
+
+<main id="tournament">
+	<ul class="round round-1">
+    <li class="spacer">&nbsp;</li>
+    <li class="game game-top winner">Colo St <span>84</span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom ">Missouri <span>72</span></li>
+		<li class="spacer">&nbsp;</li>
+		<li class="game game-top winner">Colo St <span>84</span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom ">Missouri <span>72</span></li>
+		<li class="spacer">&nbsp;</li><li class="hspacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+    <li class="game game-top winner">Colo St <span>84</span></li>
+    <li class="game game-spacer">&nbsp;</li>
+    <li class="game game-bottom ">Missouri <span>72</span></li>
+		<li class="spacer">&nbsp;</li>
+	</ul>
+	<ul class="round round-2">
+		<li class="spacer">&nbsp;</li>
+		<li class="hspacer">&nbsp;</li>
+
+		<li class="game game-top winner">Lousville <span>82</span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom ">Colo St <span>56</span></li>
+
+		<li class="spacer">&nbsp;</li>
+
+		<li class="game game-top winner">Oregon <span>74</span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom ">Saint Louis <span>57</span></li>
+		<li class="hspacer">&nbsp;</li>
+		<li class="spacer">&nbsp;</li>
+	</ul>
+	<ul class="round round-3">
+		<li class="spacer">&nbsp;</li>
+
+		<li class="game game-top winner">Lousville <span>77</span></li>
+		<li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom ">Oregon <span>69</span></li>
+
+		<li class="spacer">&nbsp;</li>
+	</ul>
+	<ul class="round round-4">
+		<li class="spacer">&nbsp;</li>
+    <li class="spacer">&nbsp;</li>
+    <li class="spacer">&nbsp;</li>
+		<li class="game game-top winner">Lousville <span>85</span></li>
+		<li class="game game-spacer">&nbsp;</li>
+    <li class="game game-spacer">&nbsp;</li>
+		<li class="game game-bottom ">Duke <span>63</span></li>
+
+		<li class="spacer">&nbsp;</li>
+	</ul>
+  <ul class="round round-5">
+		<li class="spacer">&nbsp;</li>
+    <li class="spacer">&nbsp;</li>
+		<li class="game game-top winner">Lousville <span>85</span></li>
+		<li class="spacer">&nbsp;</li>
+	</ul>
+</main>
--- frontend/views/site/tournaments.php
+++ frontend/views/site/tournaments.php
@@ -19,11 +19,19 @@ list-style:none;
 padding:0;
 }
 .round .spacer{ flex-grow:1; }
+.round .hspacer{ flex-grow:0.5; }
+.round .qspacer{ flex-grow:0.25; }
 .round .spacer:first-child,
 .round .spacer:last-child{ flex-grow:.5; }
 
 .round .game-spacer{
   flex-grow:1;
+}
+.round .hgame-spacer{
+  flex-grow:0.5;
+}
+.round .qgame-spacer{
+  flex-grow:0.25;
 }
 
 /*
@@ -37,7 +45,7 @@ line-height:1.4em;
 }
 
 li.game{
-padding-left:20px;
+padding-left: 20px;
 }
 
 li.game.winner{
@@ -54,207 +62,27 @@ li.game-spacer{
   border-right:1px solid #aaa;
   min-height:90px;
 }
+li.hgame-spacer{
+  border-right:1px solid #aaa;
+  min-height:45px;
+}
+li.qgame-spacer{
+  border-right:1px solid #aaa;
+  min-height:22px;
+}
 
 li.game-bottom{
   border-top:1px solid #aaa;
 }
 
 </style>
-
-<h1>3 TEAM DOUBLE ELIMINATION</h1>
-<main id="tournament">
-	<ul class="round round-1">
-    <li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-		<li class="game game-top winner">Colo St <span>84</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Missouri <span>72</span></li>
-
-		<li class="spacer">&nbsp;</li>
-
-		<li class="spacer">&nbsp;</li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-	<ul class="round round-2">
-		<li class="spacer">&nbsp;</li>
-
-		<li class="game game-top winner">Lousville <span>82</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Colo St <span>56</span></li>
-
-		<li class="spacer">&nbsp;</li>
-
-		<li class="game game-top winner">Oregon <span>74</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Saint Louis <span>57</span></li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-	<ul class="round round-3">
-		<li class="spacer">&nbsp;</li>
-
-		<li class="game game-top winner">Lousville <span>77</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Oregon <span>69</span></li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-	<ul class="round round-4">
-		<li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-		<li class="game game-top winner">Lousville <span>85</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-    <li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Duke <span>63</span></li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-  <ul class="round round-5">
-		<li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-		<li class="game game-top winner">Lousville <span>85</span></li>
-		<li class="spacer">&nbsp;</li>
-	</ul>
-</main>
-
-<h1>4 TEAM DOUBLE ELIMINATION</h1>
-
-<main id="tournament">
-	<ul class="round round-1">
-    <li class="spacer">&nbsp;</li>
-    <li class="game game-top winner">Colo St <span>84</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Missouri <span>72</span></li>
-		<li class="spacer">&nbsp;</li>
-		<li class="game game-top winner">Colo St <span>84</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Missouri <span>72</span></li>
-		<li class="spacer">&nbsp;</li>
-		<li class="spacer">&nbsp;</li>
-    <li class="game game-top winner">Colo St <span>84</span></li>
-    <li class="game game-spacer">&nbsp;</li>
-    <li class="game game-bottom ">Missouri <span>72</span></li>
-		<li class="spacer">&nbsp;</li>
-	</ul>
-	<ul class="round round-2">
-		<li class="spacer">&nbsp;</li>
-
-		<li class="game game-top winner">Lousville <span>82</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Colo St <span>56</span></li>
-
-		<li class="spacer">&nbsp;</li>
-
-		<li class="game game-top winner">Oregon <span>74</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Saint Louis <span>57</span></li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-	<ul class="round round-3">
-		<li class="spacer">&nbsp;</li>
-
-		<li class="game game-top winner">Lousville <span>77</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Oregon <span>69</span></li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-	<ul class="round round-4">
-		<li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-		<li class="game game-top winner">Lousville <span>85</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-    <li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">Duke <span>63</span></li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-  <ul class="round round-5">
-		<li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-		<li class="game game-top winner">Lousville <span>85</span></li>
-		<li class="spacer">&nbsp;</li>
-	</ul>
-</main>
-
-<h1>5 TEAM DOUBLE ELIMINATION</h1>
-
-<main id="tournament">
-  <ul class="round round-1">
-
-  </ul>
-	<ul class="round round-1">
-    <li class="spacer">&nbsp;</li><br><br><br><br><br>
-    <li class="game game-top winner">4 <span>84</span></li>
-    <li class="game game-spacer">&nbsp;</li>
-    <li class="game game-bottom ">5 <span>72</span></li>
-    <li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-    <!-- <li class="game game-top winner">Colo St <span>84</span></li>
-		<li class="game game-bottom ">Missouri <span>72</span></li> -->
-    <li class="game game-top winner">L1 <span>84</span></li>
-    <li class="game game-spacer">&nbsp;</li>
-    <li class="game game-bottom ">L2 <span>72</span></li>
-		<li class="spacer">&nbsp;</li>
-	</ul>
-  <ul class="round round-2">
-    <li class="game game-top winner">2 <span>84</span></li>
-    <li class="game game-spacer">&nbsp;</li>
-    <li class="game game-bottom winner">3 <span>84</span></li><br>
-    <li class="game game-top winner" >(1)<span>84</span></li>
-    <li class="game game-spacer">&nbsp;</li>
-    <li class="game game-bottom winner">1 <span>84</span></li>
-    <li class="spacer">&nbsp;</li>
-    <li class="game game-top winner">(4) <span>82</span></li>
-    <li class="game game-spacer">&nbsp;</li>
-    <li class="game game-bottom">L3 <span>82</span></li>
-  </ul>
-	<ul class="round round-2">
-		<li class="spacer">&nbsp;</li>
-		<li class="game game-top winner">(2) <span>82</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">(3) <span>56</span></li>
-		<li class="spacer">&nbsp;</li>
-		<li class="game game-top winner">L5 <span>74</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">(6) <span>57</span></li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-	<ul class="round round-3">
-		<li class="spacer">&nbsp;</li>
-
-		<li class="game game-top winner">(5) <span>77</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">(7) <span>69</span></li>
-
-		<li class="spacer">&nbsp;</li>
-	</ul>
-	<ul class="round round-4">
-		<li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-		<li class="game game-top winner">(8) <span>85</span></li>
-		<li class="game game-spacer">&nbsp;</li>
-    <li class="game game-spacer">&nbsp;</li>
-		<li class="game game-bottom ">(L8) <span>63</span></li>
-		<li class="spacer">&nbsp;</li>
-	</ul>
-  <ul class="round round-5">
-		<li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-    <li class="spacer">&nbsp;</li>
-		<li class="game game-top winner">(9 <span>85</span></li>
-		<li class="spacer">&nbsp;</li>
-		<li class="spacer">&nbsp;</li>
-	</ul>
-</main>
+<?php include ('3team.php'); ?>
+<?php include ('4team.php'); ?>
+<?php include ('5team.php'); ?>
+<?php include ('6team.php'); ?>
+<?php include ('7team.php'); ?>
+<?php include ('8team.php'); ?>
+<?php include ('9team.php'); ?>
+<?php include ('10team.php'); ?>
+<?php include ('11team.php'); ?>
+<?php include ('12team.php'); ?>
